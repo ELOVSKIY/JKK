e2ef2c2d587983334ec3a84243b868258a8c55f0
STAS <stas.elovsky@gmail.com>
2019-11-13 19:14:35 +0300

    Parser was created

---
+++ Parser.cpp
@@ -0,0 +1,5 @@
+//
+// Created by admin on 13.11.2019.
+//
+
+#include "Parser.h"
---
+++ Parser.h
@@ -0,0 +1,14 @@
+//
+// Created by admin on 13.11.2019.
+//
+
+#ifndef TEST_PARSER_H
+#define TEST_PARSER_H
+
+
+class Parser {
+
+};
+
+
+#endif //TEST_PARSER_H
